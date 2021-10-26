1855f6b4521b9ad4a16ccd6cb06fa7c713ad2396
William Sørensen <williamsorensen.work@gmail.com>
2021-10-26 13:35:21 +0200

    Misc changes

--- src/main.rs
+++ src/main.rs
@@ -67,54 +67,77 @@ macro_rules! diff_eq {
 fn mode_get() {
     let (all_set, v_set, a_set, t_set, s_set, v_0_set) = get_sets();
 
-    println!("Num of knowns");
+    let mut num: usize = 4;
+
+    let mut current_set = all_set;
+
+    while num > 0 {
+        println!("Enter name of known: v, v_0, a, t, s");
+        println!("Your current options are {:?}", current_set);
+
+        let mut s = String::new();
+        stdin().read_line(&mut s).unwrap();
+
+        let s = s.to_lowercase();
+
+        if s.contains("v") {
+            if s.contains("0") {
+                diff_eq!(current_set, v_0_set, num);
+            } else {
+                diff_eq!(current_set, v_set, num);
+            }
+        } else if s.contains("s") {
+            diff_eq!(current_set, s_set, num);
+        } else if s.contains("t") {
+            diff_eq!(current_set, t_set, num);
+        } else if s.contains("a") {
+            diff_eq!(current_set, a_set, num);
+        }
+    }
+
+    println!("Your final options are {:?}", current_set);
+}
+
+fn mode_list_formulas() {
+    println!("D. G = g * m");
+    println!("C. N = -g * m");
+    println!("C. R = -mu * g * m");
+    println!("C. B = ρ V g, where ρ = m/v the density of the fluid");
+    println!("C. L = k*v^2")
+}
+
+fn mode_convert() {
+    println!("Please select mode:");
+    println!("(1) km/h -> m/s");
+    println!("(2) m/s -> km/h");
 
     let mut s = String::new();
-
     stdin().read_line(&mut s).unwrap();
 
-    match s.trim().parse::<usize>() {
-        Ok(num) => {
-            let mut num = num;
+    let s = s.chars().next().unwrap_or('1');
 
-            let mut current_set = all_set;
+    let mut value = String::new();
+    stdin().read_line(&mut value).unwrap();
 
-            while num > 0 {
-                println!("Enter name of known: v, v_0, a, t, s");
-                println!("Your current options are {:?}", current_set);
-
-                let mut s = String::new();
-                stdin().read_line(&mut s).unwrap();
-
-                let s = s.to_lowercase();
-
-                if s.contains("v") {
-                    if s.contains("0") {
-                        diff_eq!(current_set, v_0_set, num);
-                    } else {
-                        diff_eq!(current_set, v_set, num);
-                    }
-                } else if s.contains("s") {
-                    diff_eq!(current_set, s_set, num);
-                } else if s.contains("t") {
-                    diff_eq!(current_set, t_set, num);
-                } else if s.contains("a") {
-                    diff_eq!(current_set, a_set, num);
-                }
+    match value.trim().parse::<f64>() {
+        Ok(value) => {
+            if '1' == s {
+                let value = value / 3.6;
+                println!("{}m/s", value)
+            } else if '2' == s {
+                let value = value * 3.6;
+                println!("{}km/h", value)
             }
-
-            println!("Your final options are {:?}", current_set);
         }
         Err(_) => {
-            println!("Please provide valid usize");
-            return mode_get();
+            println!("please enter valid value");
         }
     }
 }
 
 fn main() {
     loop {
-        println!("Enter mode (g)et, (e)xit");
+        println!("Enter mode (g)et, (l)ist, (c)onvert, (e)xit");
 
         let mut s = String::new();
 
@@ -124,6 +147,10 @@ fn main() {
 
         if let Some('g') = s {
             mode_get();
+        } else if let Some('l') = s {
+            mode_list_formulas();
+        } else if let Some('c') = s {
+            mode_convert();
         } else if let Some('e') = s {
             return;
         }
